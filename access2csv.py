7dff66d6e5c57a013a1a5685fddab2b34878a5d7
Tariq Alashaikh <105686563+sleepytariq@users.noreply.github.com>
2025-08-22 15:41:34 +0300

    Add an option to log lines with parsing errors

--- access2csv.py
+++ access2csv.py
@@ -5,6 +5,7 @@ import csv
 import fileinput
 import re
 import sys
+import tempfile
 from datetime import datetime
 
 
@@ -34,6 +35,14 @@ def parse_arguments() -> argparse.Namespace:
         required=True,
     )
     optional.add_argument(
+        "-e",
+        "--error",
+        type=str,
+        metavar="<PATH>",
+        help="path to save lines with parsing errors",
+        required=False,
+    )
+    optional.add_argument(
         "-h", "--help", action="help", help="show this help message and exit"
     )
     return parser.parse_args()
@@ -46,6 +55,7 @@ def main() -> None:
         with (
             fileinput.input(files=args.input) as input,
             open(args.output, "w", encoding="utf-8", newline="") as output,
+            tempfile.TemporaryFile("w+") as temperrfile,
         ):
             # fmt: off
             pattern = re.compile(r"^(?P<Host>(?:(?:\d{1,3}\.){3}\d{1,3}|([0-9a-fA-F]{0,4}\:?){2,8}))\s(?P<Clientid>[\S]+)\s(?P<Userid>[\S]+)\s\[(?P<Timestamp>\d{2}\/[A-Za-z]{3}\/\d{4}:\d{2}\:\d{2}\:\d{2}\s[+-]\d{4})\]\s\"(?P<Method>[A-Z]{3,7})\s(?P<Resource>[\S]+)\s(?P<Protocol>HTTP\/\d(?:\.\d)?)\"\s(?P<Status>\d{3})\s(?P<Size>\-|\d+)\s\"(?P<Referer>[^\"]*)\"\s\"(?P<Useragent>[^\"]*)\"$")
@@ -69,14 +79,14 @@ def main() -> None:
             )
             writer.writeheader()
 
+            errors = 0
+
             for line in input:
                 try:
                     match = pattern.match(line).groupdict()
                 except AttributeError:
-                    print(
-                        f"Error: malformed structure in {input.filename()} at line {input.filelineno()}",
-                        file=sys.stderr,
-                    )
+                    print(line, file=temperrfile, end="")
+                    errors += 1
                     continue
 
                 if match["Size"] == "-":
@@ -87,6 +97,14 @@ def main() -> None:
                 ).isoformat()
 
                 writer.writerow(match)
+
+            if args.error and errors > 0:
+                temperrfile.seek(0)
+                with open(args.error, "w") as errfile:
+                    while chunk := temperrfile.read(4096):
+                        errfile.write(chunk)
+
+            print(f"Parsed {len(args.input)} log file(s) with {errors} parsing errors")
     except (FileNotFoundError, IOError) as err:
         print(err, file=sys.stderr)
         sys.exit(1)
